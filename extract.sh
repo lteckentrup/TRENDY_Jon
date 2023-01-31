ab92d31164e14dbf8d1de81902145086ed8b559b
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2023-01-31 11:54:24 +1100

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,9 +1,14 @@
 # Read CSV file with site locations
 mapfile -t site_lat_lon < ../sitelocations.csv
 
+# Set DGVM
+DGVM='CABLE-POP'
+
+# Set var
+var='evapo'
+
 # Split each line of CSV into site name, lat, lon
 # Ignore first line (column names)
-
 for line in "${site_lat_lon[@]:1}"; do
     site=$(echo $line | cut -d, -f1)
     lat=$(echo $line | cut -d, -f2 | bc)
@@ -13,6 +18,6 @@ for line in "${site_lat_lon[@]:1}"; do
     echo ${site}
     echo ${lat}
     echo ${lon}
-    cdo -L -b F64 -selvar,evapo -remapnn,lon=${lon}_lat=${lat} \
-        CABLE-POP_S3_evapo.nc evapo/CABLE-POP_S3_${site}.nc
+    cdo -L -b F64 -selvar,${var} -remapnn,lon=${lon}_lat=${lat} \
+        ${DGVM}_S3_${var}.nc ${var}/${DGVM}_S3_${site}.nc
 done
