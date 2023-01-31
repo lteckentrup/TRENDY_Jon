8504fb25442c3c39b88d0654173bb5725c79229f
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2023-01-31 14:15:11 +1100

    Update extract.py

--- extract.py
+++ extract.py
@@ -9,23 +9,24 @@ parser.add_argument('--var', type=str, required=True)
 args = parser.parse_args()
 
 ### Read CSV with site names, lat and lon
-df_sites = pd.read_csv('sitelocations.csv')
+df_sites = pd.read_csv('../sitelocations.csv')
 
 def grab_site_data(DGVM,var):
     ### Read in file
-    fileIN = DGVM+'_S3_'+var+'.nc
-    ds = xr.open_dataset(fileIN)
+    fileIN = DGVM+'_S3_'+var+'.nc'
+    ds = xr.open_dataset(fileIN).sel(time=slice('1980','2018'))
     encoding = {'time': {'dtype': 'double'},
                 'latitude': {'dtype': 'double'},
                 'longitude': {'dtype': 'double'},
                 var: {'dtype': 'float32'}}
 
     ### Loop through all sites and write to separate netCDF
-    for site,lat,lon in zip(df_sites.Site,df_sites.latitude,df_sites.longitude):
+    for site,lat,lon in zip(df_sites.Site,df_sites.lat,df_sites.lon):
         ds_site = ds.sel(latitude=lat,longitude=lon,method='nearest')
-        fileOUT = var+'/'DGVM+'_S3_'+var+'.nc'
+        fileOUT = var+'_test/'+DGVM+'_S3_'+var+'.nc'
             
         ds_site.to_netcdf(fileOUT, encoding = encoding)
+
 
 ### Grab data
 grab_site_data(args.DGVM,args.var)
