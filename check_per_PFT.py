af40b02be8011d431f4da4945dcff83a5946ee25
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2023-02-03 12:19:41 +1100

    Create check_per_PFT.py

---
+++ check_per_PFT.py
@@ -0,0 +1,37 @@
+import xarray as xr
+import argparse
+
+### Pass DGVM and var as argument from command line
+parser = argparse.ArgumentParser()
+parser.add_argument('--DGVM', type=str, required=True)
+args = parser.parse_args()
+
+### Compare total NPP with NPP per PFT
+def pft_test(DGVM):
+
+    ### Total NPP
+    ds_total = xr.open_dataset(DGVM+'/npp/'+DGVM+'_S3_AR-SLu.nc',decode_times=False)
+
+    ### NPP per PFT
+    ds_pft = xr.open_dataset(DGVM+'/npppft/'+DGVM+'_S3_AR-SLu.nc',decode_times=False)
+
+    ### Landcover Fraction
+    ds_LCF = xr.open_dataset(DGVM+'/landCoverFrac/'+DGVM+'_S3_AR-SLu.nc',decode_times=False)
+
+    print('Total NPP ='+str(ds_total.npp.values[0].sum()))
+    print('Sum over NPP per PFT ='+str(ds_pft.npppft.values[0].flatten().sum()))
+
+    ### CLASS-CTEM and JULES-ES have LCFs that don't match carbon flux PFTs
+    if DGVM == 'CLASS-CTEM':
+        mul = ds_pft.npppft.values[0] * \
+              ds_LCF.landCoverFrac[0][:-1].values
+    elif DGVM == 'JULES-ES':
+        mul = ds_pft.npppft.values[0] * \
+              ds_LCF.landCoverFrac[0][:-4].values
+    else:
+         mul = ds_pft.npppft.values[0] * \
+               ds_LCF.landCoverFrac[0].values
+         
+    print('Sum over NPP per PFT multiplied with landcover fraction='+str(mul.flatten().sum()))
+
+pft_test(args.DGVM)
